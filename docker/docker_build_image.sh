4b86e0fa7dfaa171a3391d496e5f250430b91f34
syn-ack-zack <vengeance10@gmail.com>
2019-06-05 21:56:41 -0400

    LoRa updates

--- docker/docker_build_image.sh
+++ docker/docker_build_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 LATEST_VERSION=`git ls-remote https://github.com/syn-ack-zack/gr-lora.git | grep HEAD | cut -f 1`
-docker build -t rpp0/gr-lora --build-arg CACHEBUST=$LATEST_VERSION .
+docker build -t syn-ack-zack/gr-lora --build-arg CACHEBUST=$LATEST_VERSION .
 #docker tag rpp0/gr-lora:latest rpp0/gr-lora:$LATEST_VERSION
